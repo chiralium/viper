c5296b3bca1ef08238d5d32b23379f3e7f692fc9
Жека <be2e@yandex.ru>
2020-03-23 19:27:43 +0400

    Fix: display float constant in array

--- display.c
+++ display.c
@@ -79,8 +79,8 @@ void display_array(Array ** _array) {
     if (!_array) printf("]");
     int array_counter = 0;
     while (_array[array_counter]) {
-        if (_array[array_counter] -> type_id == INTEGER) printf("`%d`, ", *(int *)_array[array_counter] -> element);
-        else if (_array[array_counter] -> type_id == FLOAT) printf("`%d`, ", *(float *)_array[array_counter] -> element);
+        if (_array[array_counter] -> type_id == INTEGER) printf("%d, ", *(int *)_array[array_counter] -> element);
+        else if (_array[array_counter] -> type_id == FLOAT) printf("%f, ", *(float *)_array[array_counter] -> element);
         else if (_array[array_counter] -> type_id == STRING) printf("`%s`, ", (char *)_array[array_counter] -> element);
         else if (_array[array_counter] -> type_id == TOKEN) printf ("{TK:%d; `%s`}, ", ((Token *)(_array[array_counter] -> element)) -> type_id,
                                                                                        (char *)((Token *)(_array[array_counter] -> element)) -> value);
