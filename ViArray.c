561a2898bf60250a6785e9bcd2e7b2f21f1c479f
Жека <be2e@yandex.ru>
2020-04-26 22:35:43 +0400

    Fix: get_subviarray_step leak

--- ViArray.c
+++ ViArray.c
@@ -65,23 +65,22 @@ Constant * get_by_index(Constant * object, Array ** params) {
 Constant * get_subviarray_step(Constant * object, Constant * start, Constant * end, Constant * step) {
     Constant * sub_element;
     int start_indx = *(int *)(start->value); int end_indx = *(int *)(end->value); int step_val = *(int *)(step->value);
-    if (object->type_id == VIARRAY) {
-        Node * viarray = object->value; Node * subviarray = NULL;
-        Node * start_node = find_node(viarray, start_indx); Node * end_node = find_node(viarray, end_indx);
-        if (start_node == NULL || end_node == NULL) throw_arithmetical_exception(expression_as_string, VIARRAY_RANGE_EXCEPTION);
-        Node * current_node = start_node; int index = 0; Constant * copied_element;
-        while (start_indx <= end_indx || current_node != NULL) {
-            Constant * element = current_node->value;
+    Node * viarray = object->value; Node * subviarray = NULL;
+    Node * start_node = find_node(viarray, start_indx); Node * end_node = find_node(viarray, end_indx);
+    if (start_node == NULL || end_node == NULL) throw_arithmetical_exception(expression_as_string, VIARRAY_RANGE_EXCEPTION);
+    Node * current_node = start_node; int index = 0; Constant * copied_element;
+    while (start_indx <= end_indx || current_node != NULL) {
+        Constant * element = current_node->value;
 
-            copied_element = new_constant(element->type_id, copy_data(element->value, element->type_id));
-            if (!is_simple_data(element->type_id)) copied_element->origin = element->value; // is the element is a complex data that stored into namespace set the origin
+        copied_element = new_constant(element->type_id, copy_data(element->value, element->type_id));
+        if (!is_simple_data(element->type_id)) copied_element->origin = element->value; // is the element is a complex data that stored into namespace set the origin
 
-            Node * subviarray_node = new_node(index++, copied_element);
-            (subviarray == NULL) ? subviarray = insert_node(subviarray, subviarray_node) : insert_node(subviarray, subviarray_node);
-            start_indx += step_val; current_node = find_node(viarray, start_indx);
-        }
-        sub_element = new_constant(VIARRAY, subviarray);
+        Node * subviarray_node = new_node(index++, copied_element);
+        (subviarray == NULL) ? subviarray = insert_node(subviarray, subviarray_node) : insert_node(subviarray, subviarray_node);
+        start_indx += step_val; current_node = find_node(viarray, start_indx);
     }
+    sub_element = new_constant(VIARRAY, subviarray);
+    memory_table = append(memory_table, MEMORY_ELEMENT, new_memory_element(VIARRAY, subviarray, "ViArray.c/get_subviarray_step"));
     return sub_element;
 }
 
