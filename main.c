b958ab1922a59ff7d17ab8e7c53484680749e96e
Жека <be2e@yandex.ru>
2020-05-04 00:06:35 +0400

    Add: speed measurement

--- main.c
+++ main.c
@@ -1,5 +1,9 @@
+#include <time.h>
 #include "run.h"
 
 int main() {
+    clock_t tic = clock();
     run();
+    clock_t tac = clock();
+    printf("\n%f seconds\n", (double)(tac - tic) / CLOCKS_PER_SEC);
 }
